cd155d082bc6288ba5599f432a003fb46c978a0b
esakki-cyber <131432894+esakki-cyber@users.noreply.github.com>
2024-01-29 17:44:55 +0530

    first-cm-1

---
+++ public/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-yCkaFvGjwlhpcCC-b1UEQLCbk9mWbZE",
+  authDomain: "quality-menswear.firebaseapp.com",
+  projectId: "quality-menswear",
+  storageBucket: "quality-menswear.appspot.com",
+  messagingSenderId: "569606697426",
+  appId: "1:569606697426:web:23dc04741a01d85be3580b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
